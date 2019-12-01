7d025ed684809fac64605b0dd17d7458203921f3
Remington Anton Amalanathan <remigntonanton@gmail.com>
2019-12-01 04:10:31 +0000

    Add loading ideas page

--- startup_io/lib/loadingData.dart
+++ startup_io/lib/loadingData.dart
@@ -1,22 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class LoadingData extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,
-      width: 300.0,
-      height: 300.0,
-      child: SpinKitFadingCircle(
-        itemBuilder: (_, int index) {
-          return DecoratedBox(
-            decoration: BoxDecoration(
-              color: index.isEven ? Colors.tealAccent[100] : Colors.greenAccent[100],
-            ),
-          );
-        },
-        size: 150.0,
+    return Scaffold(
+      body: Container(
+          color: Colors.black,
+          //width: 300.0,
+          //height: 300.0,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 200,
+              child: SpinKitFadingCircle(
+                itemBuilder: (_, int index) {
+                  return DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: index.isEven ? Colors.tealAccent[100] : Colors
+                          .greenAccent[100],
+                    ),
+                  );
+                },
+                size: 150.0,
+              ),
+              ),
+              SizedBox(
+                child: FadeAnimatedTextKit(
+                    text: [
+                      "Contacting venture capitalists",
+                      "Hiring WeWork space",
+                      "Using unstable technology"
+                    ],
+                    textStyle: TextStyle(
+                        fontSize: 30.0,
+                        fontFamily: "Canterbury",
+                      color: Colors.tealAccent[100],
+                    ),
+                    textAlign: TextAlign.center,
+                    alignment: AlignmentDirectional
+                        .center // or Alignment.topLeft
+                ),
+              ),
+            ],
+          )
       ),
     );
   }
